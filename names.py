15021a1c6a5fc49007b55a748a3e838d3b4be20c
Brian Perrett <perrettbrian@gmail.com>
2023-03-25 19:28:15 -0600

    Use some regex.

--- names.py
+++ names.py
@@ -1,60 +1,51 @@
 import requests
 from urllib3 import disable_warnings
 from urllib.parse import quote
-import platform
 import psutil
 import base64
 import os
-from functools import cached_property
 import logging
+import re
 
 
 logger = logging.getLogger(__name__)
 disable_warnings()
 RIOTCLIENT_API = "https://127.0.0.1:{}"
+LCU_NAME = 'LeagueClientUx'
 
 
 class LCU:
     def __init__(self):
+        self.process_args = {}
         self.get_lcu_args()
 
-    @cached_property
-    def lcu_name(self):
-        system = platform.system()
-        return "LeagueClientUx" if system in ("Darwin", "Linux") else "LeagueClientUx.exe"
-
     def is_lcu_available(self):
-        return self.lcu_name in (p.name() for p in psutil.process_iter())
+        for p in psutil.process_iter():
+            if LCU_NAME in p.name():
+                return True
+        return False
 
     def get_lcu_args(self):
         if not self.is_lcu_available():
-            logger.warn(f"No {self.lcu_name} found. Login to an account and try again.")
+            logger.warn(f"No {LCU_NAME} found. Login to an account and try again.")
             return
 
+        matcher = re.compile(r'^--([^=]+)=(.+)$')
         for p in psutil.process_iter():
-            if p.name() == self.lcu_name:
+            if LCU_NAME in p.name():
                 args = p.cmdline()
                 for a in args:
-                    if "--region=" in a:
-                        self.region = a.split("--region=", 1)[1].lower()
-                    if "--remoting-auth-token=" in a:
-                        self.auth_token = a.split("--remoting-auth-token=", 1)[1]
-                        self.lcu_session_token = base64.b64encode(
-                            ("riot:" + self.auth_token).encode("ascii")
-                        ).decode("ascii")
-                    if "--app-port" in a:
-                        self.app_port = a.split("--app-port=", 1)[1]
-                    if "--riotclient-auth-token=" in a:
-                        self.riotclient_auth_token = a.split(
-                            "--riotclient-auth-token=", 1
-                        )[1]
-                        self.riotclient_session_token = base64.b64encode(
-                            ("riot:" + self.riotclient_auth_token).encode("ascii")
-                        ).decode("ascii")
-                    if "--riotclient-app-port=" in a:
-                        self.riotclient_app_port = a.split("--riotclient-app-port=", 1)[
-                            1
-                        ]
+                    if match := matcher.match(a):
+                        name, value = match.groups()
+                        self.process_args[name] = value
+                break
+
+        self.lcu_session_token = base64.b64encode(
+            ("riot:" + self.process_args['remoting-auth-token']).encode("ascii")
+        ).decode("ascii")
+        self.riotclient_session_token = base64.b64encode(
+            ("riot:" + self.process_args['riotclient-auth-token']).encode("ascii")
+        ).decode("ascii")
 
         self.lcu_headers = {
             "Content-Type": "application/json",
@@ -71,19 +62,17 @@ class LCU:
 
     def get_participants(self):
         logger.info("Getting Participants")
-        url = RIOTCLIENT_API.format(self.riotclient_app_port) + "/chat/v5/participants/champ-select"
+        url = RIOTCLIENT_API.format(self.process_args['riotclient-app-port']) + "/chat/v5/participants/champ-select"
         r = requests.get(url, headers=self.riotclient_headers, verify=False)
         participants = r.json()["participants"]
         return participants
 
     def get_participant_names(self):
-        participants = self.get_participants()
-        if participants:
-            return [x["name"] for x in participants]
-        return []
+        participants = self.get_participants() or []
+        return [x["name"] for x in participants]
 
     def get_porofessor_link(self):
-        base = f"https://porofessor.gg/pregame/{self.region}/"
+        base = f"https://porofessor.gg/pregame/{self.process_args['region']}/"
         names = self.get_participant_names()
         if names:
             param = ",".join(quote(name) for name in names)
